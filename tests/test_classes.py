2465039349130c95b9a5d95cdc1e73d67d674075
vyacheslav <vyacheslav_kazantsev@list.ru>
2024-04-11 18:54:42 +0500

    added tests for counters

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -12,6 +12,12 @@ def test_product(product_sapog):
     assert product_sapog.description == 'shoe'
     assert product_sapog.price == '12,99'
     assert product_sapog.availability == '2'
+    assert product_sapog.products_count == 1
+
+
+"""@pytest.fixture
+def test_categories_count():
+    return classes.Category(categories_count = 1)"""
 
 
 @pytest.fixture
@@ -23,3 +29,4 @@ def test_category(category_obuv):
     assert category_obuv.name == 'obuv'
     assert category_obuv.description == 'on your feet'
     assert category_obuv.products == ['1', '2']
+    assert category_obuv.categories_count == 1
