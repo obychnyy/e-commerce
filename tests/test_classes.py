2856fadf0a4cfc82f7c6f4cefd3a4ad10a039833
vyacheslav <vyacheslav_kazantsev@list.ru>
2025-07-29 15:04:02 +0500

    added test

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -4,31 +4,52 @@ from utils import classes
 
 @pytest.fixture
 def product_sapog():
-    return classes.Product('sapog', 'shoe', '12,99', '2')
-
-
-def test_product(product_sapog):
-    assert product_sapog.name == 'sapog'
-    assert product_sapog.description == 'shoe'
-    assert product_sapog.price == '12,99'
-    assert product_sapog.availability == '2'
-    assert product_sapog.products_count == 1
-
+    product = classes.Product('sapog', 'shoe', 12.99, 2)
+    try:
+        yield product
+    finally:
+        product._confirm_lower_price = False
+        product.priced = 12.99
 
 @pytest.fixture
-def category_obuv():
-    return classes.Category('obuv', 'on your feet')
+def product_botinok():
+    return classes.Product('botinok', 'another shoe', 17.85, 10)
 
+def test_product(product_sapog, product_botinok):
+    assert product_sapog.name == 'sapog'
+    assert product_sapog.description == 'shoe'
+    assert product_sapog.price == 12.99
+    assert product_sapog.availability == 2
+    assert product_sapog + product_botinok == 204.48
+    assert str(product_sapog) == 'sapog, 12.99 руб. Остаток: 2 шт.'
+
+def test_priced_setter(product_sapog):
+    product_sapog.priced = 15.99
+    assert product_sapog.priced == 15.99
+    product_sapog._confirm_lower_price = True
+    product_sapog.priced = 10.99
+    product_sapog._confirm_lower_price = False
+    with pytest.raises(ValueError):
+        product_sapog.priced = -10
+    product_sapog._confirm_lower_price = False
+    with pytest.raises(TypeError):
+        product_sapog.priced = 'string'
+    product_sapog._confirm_lower_price = False
+    with pytest.raises(ValueError):
+        product_sapog.priced = 5.99
+
+@pytest.fixture
+def category_obuv(product_sapog, product_botinok):
+    x = classes.Category('obuv', 'on your feet')
+    x.add_product(product_sapog)
+    x.add_product(product_botinok)
+    return x
 
 def test_category(category_obuv):
     assert category_obuv.name == 'obuv'
     assert category_obuv.description == 'on your feet'
     assert category_obuv.categories_count == 1
+    assert str(category_obuv) == 'obuv - общее количество продуктов: 12'
 
-
-"""@pytest.fixture
-def reprice_sapog():
-    return classes.Product.set_price(11,99)
-
-def test_product_reprice(reprice_sapog):
+"""def test_product_reprice(reprice_sapog):
     assert reprice_sapog.out == '11,99'"""
--- utils/classes.py
+++ utils/classes.py
@@ -8,20 +8,20 @@ class Category:
     out: str
     categories_count = 0
     products_count = 0
-    quantity = 0
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.__products = []
+        self.products = []
         self.out = ""
-        Category.categories_count += 1
+        self.categories_count += 1
+        self.quantity = 0
 
     def __str__(self):
-        return f'{self.name}, количество продуктов: {self.quantity}'
+        return f'{self.name} - общее количество продуктов: {self.quantity}'
 
     def add_product(self, NewProduct):
-        self.__products.append(NewProduct)
+        self.products.append(NewProduct)
         self.products_count = len(self.products)
         self.quantity += NewProduct.availability
 
@@ -44,9 +44,10 @@ class Product:
         self.description = description
         self.price = price
         self.availability = availability
+        self._confirm_lower_price = False
 
     def __str__(self):
-        return f'{self.name}, {self.price} руб. Остаток: {self.availability}'
+        return f'{self.name}, {self.price} руб. Остаток: {self.availability} шт.'
 
     def __add__(self, other):
         return (self.price * self.availability) + (other.price * other.availability)
@@ -56,20 +57,18 @@ class Product:
         return cls(**product_data)
 
     @property
-    def price(self):
+    def priced(self):
         return self.price
 
-    @price.setter
-    def price(self, price):
-        confirm = ""
-        if price > 0:
-            if self.price > price:
-                print("Вы действительно хотите понизить цену?")
-                print("y/n")
-                confirm = input()
-                if confirm == 'y':
-                    self.price = price
-            else:
-                self.price = price
-        else:
-            print('Вы уверены, что это цена?')
+    @priced.setter
+    def priced(self, new_price):
+        confirm = ''
+        if not isinstance(new_price, (int, float)):
+            raise TypeError('Цена должна быть числом')
+        if new_price <= 0:
+            raise ValueError('Цена должна быть положительной')
+
+        if new_price < self.price:
+            if not self._confirm_lower_price:
+                raise ValueError("Требуется подтверждение понижения цены")
+        self.price = new_price
