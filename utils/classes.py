5e286a2821d3c836476215a7c173a93c9f55b6d1
obychnyy <104889977+obychnyy@users.noreply.github.com>
2025-07-30 12:35:05 +0500

    Revert "Develop"

--- utils/classes.py
+++ utils/classes.py
@@ -1,38 +1,14 @@
-import setter
-
-
 class Category:
     name: str
     description: str
     products: list
-    out: str
     categories_count = 0
-    products_count = 0
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = []
-        self.out = ""
-        self.categories_count += 1
-        self.quantity = 0
-
-    def __str__(self):
-        return f'{self.name} - общее количество продуктов: {self.quantity}'
-
-    def add_product(self, NewProduct):
-        if isinstance(NewProduct, LawnGrass) or isinstance(NewProduct, Smartphone) or isinstance(NewProduct, Product):
-            self.products.append(NewProduct)
-            self.products_count = len(self.products)
-            self.quantity += NewProduct.availability
-        else:
-            raise TypeError('Можно добавлять только объекты класса Product или его подклассов')
-
-"""    @property
-    def printered(self):
-        for i in self.__products:
-            self.out += (f"{i.name}, {i.price}руб. Остаток: {i.availability}шт." + "\n")
-        return self.out"""
+        self.products = products
+        Category.categories_count += 1
 
 
 class Product:
@@ -40,57 +16,11 @@ class Product:
     description: str
     price: float
     availability: int
-    product_data: dict
+    products_count = 0
 
     def __init__(self, name, description, price, availability):
         self.name = name
         self.description = description
         self.price = price
         self.availability = availability
-        self._confirm_lower_price = False
-
-    def __str__(self):
-        return f'{self.name}, {self.price} руб. Остаток: {self.availability} шт.'
-
-    def __add__(self, other):
-        if type(self) != type(other):
-            raise TypeError("Нельзя складывать товары разных видов")
-        return (self.price * self.availability) + (other.price * other.availability)
-
-    @classmethod
-    def new_product(cls, product_data: dict):
-        return cls(**product_data)
-
-    @property
-    def priced(self):
-        return self.price
-
-    @priced.setter
-    def priced(self, new_price):
-        confirm = ''
-        if not isinstance(new_price, (int, float)):
-            raise TypeError('Цена должна быть числом')
-        if new_price <= 0:
-            raise ValueError('Цена должна быть положительной')
-
-        if new_price < self.price:
-            if not self._confirm_lower_price:
-                raise ValueError("Требуется подтверждение понижения цены")
-        self.price = new_price
-
-
-class Smartphone(Product):
-    def __init__(self, efficiency, model, memory, color, name, description, price, availability):
-        super().__init__(name, description, price, availability)
-        self.efficiency = efficiency
-        self.model = model
-        self.memory = memory
-        self.color = color
-
-
-class LawnGrass(Product):
-    def __init__(self, country, germination_period, color, name, description, price, availability):
-        super().__init__(name, description, price, availability)
-        self.country = country
-        self.germination_period = germination_period
-        self.color = color
+        Product.products_count += 1
---
+++ utils/functions.py
@@ -0,0 +1,4 @@
+import classes
+
+def make_product():
+
